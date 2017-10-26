a23366e962c506327a690113cfda0238a809d98a
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-10-26 13:07:09 -0700

    fixing hls constructor

--- test/hls.js
+++ test/hls.js
@@ -9,9 +9,11 @@ describe('hls', () => {
         const fn = hls(
             (a, b, c) => (a + b) - ((5 + 7) + c)
         );
-        const g = fn('a', 'b', 'c').state.root;
-        console.log(fhyperDot(g));
+        const resEdge = fn('a', 'b', 'c');
+        // resEdge(_g({name: '_res'}));
+        const g = resEdge.state.root;
         // expect(g).to.deep.equal(['_sub', ['_add', 'a', 'b'], ['_add', 12, 'c']]);
+        console.log(fhyperDot(g));
         done();
     });
 });
