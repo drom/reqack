c2c8c56a288b529c15b9a667a95282abf4986a38
drom <alex.drom@gmail.com>
2018-02-09 18:25:09 -0800

    bunch of fixes

--- test/basic.js
+++ test/basic.js
@@ -6,74 +6,76 @@ const fhyper = require('../lib/fhyper');
 const fhyperDot = require('../lib/fhyper-dot');
 const fhyperV = require('../lib/fhyper-verilog');
 
+function dump (g, name, done) {
+    fs.outputFile(
+        path.resolve('build', name + '.dot'),
+        fhyperDot(g),
+        () => fs.outputFile(
+            path.resolve('build', name + '.v'),
+            fhyperV(g),
+            done
+        )
+    );
+}
+
+const perEdgeSetWidth = w => e =>
+    (e.label = (typeof e.label !== 'object') ? {} : e.label).width = w;
+
 describe('basic', () => {
 
     it('add2', done => {
         const g = fhyper();
-        const i0 = g()
-            , i1 = g()
-            , o = g()
-            , add2 = g('add');
-
+        const i0 = g(), i1 = g(), o = g(), add2 = g('add');
         i0()(add2);
         i1()(add2);
         add2()(o);
-
-        g.edges.forEach(e => e.label = {width: 8});
-
-        fs.outputFile(path.resolve('build', 'add2.dot'), fhyperDot(g), () => {
-            fs.outputFile(path.resolve('build', 'add2.v'), fhyperV(g), done);
-        });
+        g.edges.forEach(perEdgeSetWidth(8));
+        dump(g, 'add2', done);
     });
 
     it('radix2', done => {
-        const g = fhyper();
-
+        const g = fhyper('datapath');
         // construct functional nodes
         const add = g('add'), sub = g('sub');
 
         // construct interconnect
-        g()({})(add)(sub);
-        g()({})(add)(sub);
-        add({capacity: 1})(g());
+        g()()(add)(sub);
+        g()()(add)(sub);
+        add()(g());
         sub({capacity: 1})(g());
+        g.edges.forEach(perEdgeSetWidth(16));
 
-        // set edge width
-        g.edges.forEach(e => e.label.width = 16);
-
-        fs.outputFile(path.resolve('build', 'radix2.dot'), fhyperDot(g), () => {
-            fs.outputFile(path.resolve('build', 'radix2.v'), fhyperV(g), done);
-        });
+        dump(g, 'radix2', done);
     });
 
     it('retiming', done => {
-        const G = fhyper();
+        const gg = fhyper();
 
         // construct circuit
-        const a = G('a');
-        const b = G('b');
-        const c = G('c');
-        const d = G('d');
-        const e = G('e');
-        const f = G('f');
-        const g = G('g');
-        const h = G('h');
+        const a = gg('a');
+        const b = gg('b');
+        const c = gg('c');
+        const d = gg('d');
+        const e = gg('e');
+        const f = gg('f');
+        const g = gg('g');
+        const h = gg('h');
 
         a({capacity: 1})(b);
         b({capacity: 1})(c);
         c({capacity: 1})(d);
         d({capacity: 1})(e);
 
-        b({})(h);
-        c({})(g);
-        d({})(f);
-        e({})(f);
-        f({})(g);
-        g({})(h);
-        h({})(a);
+        b()(h);
+        c()(g);
+        d()(f);
+        e()(f);
+        f()(g);
+        g()(h);
+        h()(a);
 
-        fs.outputFile(path.resolve('build', 'retiming.dot'), fhyperDot(G), () => {
-            fs.outputFile(path.resolve('build', 'retiming.v'), fhyperV(G), done);
-        });
+        gg.edges.forEach(perEdgeSetWidth(16));
+
+        dump(gg, 'retiming', done);
     });
 });
