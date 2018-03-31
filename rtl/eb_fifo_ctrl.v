b0c371523ca83d7642e584cf0c1ce1516af1530c
drom <alex.drom@gmail.com>
2018-03-30 17:53:52 -0700

    ebfifo cleanup

--- rtl/eb_fifo_ctrl.v
+++ rtl/eb_fifo_ctrl.v
@@ -1,44 +1,41 @@
-module eb_fifo_ctrl
-  (input wire t_req,
-   output wire                       t_ack,
-   output wire                       i_req,
-   input wire                        i_ack,
-   output reg [<%= depthlog2-1 %>:0] wr_ptr,
-   output reg [<%= depthlog2-1 %>:0] rd_ptr,
-   output wire                       wen,
-   output wire                       ren,
-   input wire                        clk, reset_n
-   );
+module eb_fifo_ctrl #(
+    parameter DEPTHMO = 4'd15,
+    parameter DEPTHLOG2MO = 3
+) (
+    input   t_0_req,
+    output  t_0_ack,
+    output  i_0_req,
+    input   i_0_ack,
+    output reg [DEPTHLOG2MO : 0] wr_ptr,
+    output reg [DEPTHLOG2MO : 0] rd_ptr,
+    output  wen, ren,
+    input   clk, reset_n
+);
 
-   reg [<%= depthlog2 -1:0] status_cnt;
-   
-   assign t_ack=!(status_cnt==<%= depth-1 %>);
-   assign ren=i_req && i_ack;
-   assign wen=t_req && t_ack;
+reg [DEPTHLOG2MO : 0] status_cnt;
 
-   always @(posedge clk or negedge reset_n)
-     if(~reset_n) i_req<=0;
-     else if(status_cnt==) i_req<=0;
-     else if((i_req && i_ack) && status_cnt==1) i_req<=0;
-     else i_req<=1;
+assign t_0_ack = !(status_cnt == DEPTHMO);
+assign ren = i_0_req && i_0_ack;
+assign wen = t_0_req && t_0_ack;
 
-   always @(posedge clk or negedge reset_n)
-     if(~reset_n) wr_ptr<=0;
-     else if (t_req && t_ack) wr_ptr<=(wpr_ptr==<%= depth-1 %>)?0:wr_ptr+1;
+always @(posedge clk or negedge reset_n)
+    if (~reset_n) i_0_req <= 1'b0;
+    else if (status_cnt == DEPTHMO) i_0_req <= 0;
+    else if ((i_0_req && i_0_ack) && (status_cnt == 1)) i_0_req <= 0;
+    else i_0_req <= 1;
 
-   always @(posedge clk or negedge reset_n)
-     if(~reset_n) rd_ptr<=0;
-     else if(i_req && i_ack) 
-       rd_ptr<=(rd_ptr==<%= depth-1 %> && status_cnt !=0)?0:rd_ptr+1;
+always @(posedge clk or negedge reset_n)
+    if (~reset_n) wr_ptr <= 0;
+    else if (t_0_req && t_0_ack) wr_ptr <= (wr_ptr == DEPTHMO) ? 0 : (wr_ptr + 1);
 
-   always @(posedge clk or negedge reset_n)
-     if(~reset_n) status_cnt<=0;
-     else if((i_req && i_ack) && (t_req && t_ack))
-       status_cnt<= status_cnt;
-     else if(i_req && i_ack && status_cnt != 0)
-       status_cnt<= status_cnt-1;
-     else if(t_req && t_ack)
-       status_cnt<= status_cnt+1;
-   
+always @(posedge clk or negedge reset_n)
+    if (~reset_n) rd_ptr <= 0;
+    else if (i_0_req && i_0_ack) rd_ptr <= (((rd_ptr == DEPTHMO) && (status_cnt != 0)) ? 0 : (rd_ptr + 1));
 
-endmodule   
+always @(posedge clk or negedge reset_n)
+    if (~reset_n) status_cnt <= 0;
+    else if ((i_0_req && i_0_ack) && (t_0_req && t_0_ack)) status_cnt <= status_cnt;
+    else if (i_0_req && i_0_ack && (status_cnt != 0)) status_cnt <= status_cnt - 1;
+    else if (t_0_req && t_0_ack) status_cnt <= status_cnt + 1;
+
+endmodule
