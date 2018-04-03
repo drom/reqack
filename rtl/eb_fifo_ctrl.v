253a83cfb83affe75ecda4fcf2089a0b4e4861d3
Ameet Gohil <ameet@siglabs.com>
2018-04-03 15:57:58 -0700

    fix ebfifo

--- rtl/eb_fifo_ctrl.v
+++ rtl/eb_fifo_ctrl.v
@@ -13,15 +13,15 @@ module eb_fifo_ctrl #(
 );
 
 reg [DEPTHLOG2MO : 0] status_cnt;
-reg [DEPTHLOG2MO : 0] rd_ptr_calc;
+reg [DEPTHLOG2MO : 0] q_rd_ptr;
 
 assign t_0_ack = !(status_cnt == DEPTHMO);
-assign ren = i_0_req && i_0_ack;
+assign ren = 1;
 assign wen = t_0_req && t_0_ack;
 
 always @(posedge clk or negedge reset_n)
     if (~reset_n) i_0_req <= 1'b0;
-    else if (status_cnt == DEPTHMO) i_0_req <= 0;
+    else if (status_cnt == 0) i_0_req <= 0;
     else if ((i_0_req && i_0_ack) && (status_cnt == 1)) i_0_req <= 0;
     else i_0_req <= 1;
 
@@ -29,13 +29,13 @@ always @(posedge clk or negedge reset_n)
     if (~reset_n) wr_ptr <= 0;
     else if (t_0_req && t_0_ack) wr_ptr <= (wr_ptr == DEPTHMO) ? 0 : (wr_ptr + 1);
 
+   assign rd_ptr=(i_0_req & i_0_ack)?(((q_rd_ptr == DEPTHMO) && (status_cnt != 0)) ? 0 : q_rd_ptr + 1):q_rd_ptr;
+   
 always @(posedge clk or negedge reset_n)
     if (~reset_n) begin
-       rd_ptr <= 0;
-       rd_ptr_calc <= 0;
+       q_rd_ptr <= 0;
     end else begin
-       rd_ptr<=(status_cnt!=0)?rd_ptr_calc+1:rd_ptr_calc;
-       if (i_0_req && i_0_ack) rd_ptr_calc <= (((rd_ptr_calc == DEPTHMO) && (status_cnt != 0)) ? 0 : (rd_ptr_calc + 1));
+       if (i_0_req && i_0_ack) q_rd_ptr <= (((q_rd_ptr == DEPTHMO) && (status_cnt != 0)) ? 0 : (q_rd_ptr + 1));
     end
 
 always @(posedge clk or negedge reset_n)
