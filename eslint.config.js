1b46e2cf90198c33543df19596d2698a4c9081bb
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2026-07-15 16:47:38 -0700

    :arrow_up: dependencies

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = [
+  require('@drom/eslint-config'),
+  {
+    files: ['test/**/*.js'],
+    languageOptions: {
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        before: 'readonly',
+        after: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly'
+      }
+    }
+  }
+];
